fd3fd3e813a36be966c4402149a8aa8f209f1d23
Andres Mendez <andres.f.mendez123@gmail.com>
2022-07-16 19:35:45 -0300

    commit de prueba

---
+++ src/main/java/com/coderhouse/ProyectoFinalMendezFlorez/ProyectoFinalMendezFlorezApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse.ProyectoFinalMendezFlorez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalMendezFlorezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalMendezFlorezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderhouse/ProyectoFinalMendezFlorez/ProyectoFinalMendezFlorezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.ProyectoFinalMendezFlorez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalMendezFlorezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
